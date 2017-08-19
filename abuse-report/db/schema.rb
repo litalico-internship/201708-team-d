5c11efa09825fb3562ff4e03a756e8340def5886
Takehiro-Matsuo <g1545367@cc.kyoto-su.ac.jp>
2017-08-20 02:02:53 +0900

    bootstrapの導入と簡易レイアウトの完成

--- abuse-report/db/schema.rb
+++ abuse-report/db/schema.rb
@@ -10,47 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170819114734) do
-
-  create_table "notice_signs", force: :cascade do |t|
-    t.integer  "notice_id"
-    t.integer  "sign_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["notice_id"], name: "index_notice_signs_on_notice_id"
-    t.index ["sign_id"], name: "index_notice_signs_on_sign_id"
-    t.index [nil, nil], name: "index_notice_signs_on_notice_and_sign", unique: true
-  end
-
-  create_table "notices", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "status_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "school_id"
-    t.index ["school_id"], name: "index_notices_on_school_id"
-    t.index ["status_id"], name: "index_notices_on_status_id"
-    t.index ["user_id"], name: "index_notices_on_user_id"
-  end
+ActiveRecord::Schema.define(version: 20170819084316) do
 
   create_table "report_signs", force: :cascade do |t|
     t.integer  "report_id"
-    t.integer  "sign_id"
+    t.integer  "signs_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["report_id", "sign_id"], name: "index_report_signs_on_report_id_and_sign_id", unique: true
+    t.index ["report_id", nil], name: "index_report_signs_on_report_id_and_sign_id", unique: true
     t.index ["report_id"], name: "index_report_signs_on_report_id"
-    t.index ["sign_id"], name: "index_report_signs_on_sign_id"
+    t.index ["signs_id"], name: "index_report_signs_on_signs_id"
   end
 
   create_table "reports", force: :cascade do |t|
     t.string   "ls"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "schools", force: :cascade do |t|
-    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
