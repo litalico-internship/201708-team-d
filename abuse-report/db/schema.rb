4baedbc47932f6eb92ea7ffcaba402aeb08e0b6f
intern <intern@MAC20170808-2.local>
2017-08-19 20:58:31 +0900

    edit schema

--- abuse-report/db/schema.rb
+++ abuse-report/db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20170819114734) do
-=======
-ActiveRecord::Schema.define(version: 20170819090153) do
->>>>>>> dbac95d0b2e925b5500766d8d87837a0a2cae87c
 
   create_table "notice_signs", force: :cascade do |t|
     t.integer  "notice_id"
@@ -27,19 +23,12 @@ ActiveRecord::Schema.define(version: 20170819090153) do
   end
 
   create_table "notices", force: :cascade do |t|
-<<<<<<< HEAD
-=======
-    t.string   "school"
->>>>>>> dbac95d0b2e925b5500766d8d87837a0a2cae87c
     t.integer  "user_id"
     t.integer  "status_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-<<<<<<< HEAD
     t.integer  "school_id"
     t.index ["school_id"], name: "index_notices_on_school_id"
-=======
->>>>>>> dbac95d0b2e925b5500766d8d87837a0a2cae87c
     t.index ["status_id"], name: "index_notices_on_status_id"
     t.index ["user_id"], name: "index_notices_on_user_id"
   end
