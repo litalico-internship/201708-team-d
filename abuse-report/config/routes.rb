4159be09cd040be5dd7184e467100330410301e1
Takehiro-Matsuo <g1545367@cc.kyoto-su.ac.jp>
2017-08-19 18:04:56 +0900

    set routing

--- abuse-report/config/routes.rb
+++ abuse-report/config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#index'
+
+  resources :home do
+  end
+
+  resources :reports do
+  end
+
+  resources :notices do
+  end
+
 end
--- abuse-report/db/schema.rb
+++ abuse-report/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170819075430) do
+ActiveRecord::Schema.define(version: 20170819081057) do
 
   create_table "reports", force: :cascade do |t|
     t.string   "ls"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20170819075430) do
   end
 
   create_table "signs", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "statuses", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
