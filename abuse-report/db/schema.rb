5ab64fca9fe6133fe4d524a23d5666bf14fd9e42
intern <intern@MAC20170808-2.local>
2017-08-19 16:56:21 +0900

    Genrated Report model

--- abuse-report/db/schema.rb
+++ abuse-report/db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170819073339) do
+ActiveRecord::Schema.define(version: 20170819074858) do
 
   create_table "signs", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "session_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
